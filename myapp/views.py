c614b5746c444eab738fc05c96f8388236362aa3
HailerSH <hsernah.dev@gmail.com>
2025-06-12 21:46:40 -0500

    Bug Fix in prediction modal

--- myapp/views.py
+++ myapp/views.py
@@ -7,14 +7,13 @@ def intro(request):
 
 def predict(request):
     """ Handles prediction form and returns scorecard """
-    probabilities = None
-    predictions = None
+    probability = None
 
     if request.method == 'POST':
         income = float(request.POST.get('income'))
         age = int(request.POST.get('age'))
         loan_amount = float(request.POST.get('loan_amount'))
 
-        probabilities, predictions = main(total_rec_prncp=0.1457, funded_amnt=0.1333, total_pymnt_inv=-0.2496)
+        probability, _ = main(total_rec_prncp=0.1457, funded_amnt=0.1333, total_pymnt_inv=-0.2496)
 
-    return render(request, 'myapp/predict.html', {'probabilities': probabilities, 'predictions': predictions})
+    return render(request, 'myapp/predict.html', {'probability': round(probability[0][0], 2) if probability else None})
